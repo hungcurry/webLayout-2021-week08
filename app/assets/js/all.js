dc688728d55c653560a06609fcf0055e77007300
hungcurry <ooopp42@gmail.com>
2021-08-25 16:34:00 +0800

    [ add ] course && scheme  && recommend  完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,20 @@
+var swiper = new Swiper(".reSwiper", {
+  slidesPerView: "auto",
+  slidesPerColumn: 3,
+  spaceBetween: 30,
+  breakpoints: {
+    768:{
+      spaceBetween: 15,
+      slidesPerColumn: 2,
+    },
+    992:{
+      spaceBetween: 30,
+      slidesPerColumn: 2,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
