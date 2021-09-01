852874af819aba6dab4ae18aa2f85ea4cfe5d22c
hungcurry <ooopp42@gmail.com>
2021-09-01 21:09:32 +0800

    [ edit ] reservation && _program 合併一頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -47,6 +47,7 @@ let recommendSwiper = new Swiper(".recommendSwiper", {
   },
 });
 
+
 // 課程介紹
 let courseIntroduceSwiper = new Swiper(".courseIntroduceSwiper", {
   slidesPerView: "auto",
@@ -64,7 +65,83 @@ let courseIntroduceSwiper = new Swiper(".courseIntroduceSwiper", {
   },
 });
 
-// 選擇課程階級
+
+// 瑜珈空間
+let envRecommendSwiper = new Swiper(".envRecommendSwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+});
+
+
+// 立即預約
+const allBtn = document.querySelectorAll('.js-schemeBtn');
+const reserveSwiper = document.querySelector('.reservationSwiper');
+const allItem = document.querySelectorAll('.js-reservationSwiperItem');
+const courseTitle = document.querySelector('.js-courseTitle');
+const choose = document.querySelector('.js-choose');
+let courseStr = "";
+
+if(allBtn && allItem && choose){
+  choose.classList.add('d-none');
+  allBtn.forEach((item)=>{
+    item.addEventListener('click', reserveCollapseFn);
+  });
+  allItem.forEach((item)=>{
+    item.addEventListener('click', reserveSwiperFn)
+  });  
+}
+// collapse
+function reserveCollapseFn(){
+  let clickItem = this ;
+  let clickCard = clickItem.parentElement.parentElement;
+  let isExpanded = clickItem.getAttribute('aria-expanded');
+
+  if(isExpanded === 'true'){ 
+    choose.classList.remove('d-none');
+    allBtn.forEach((item)=>{
+      item.setAttribute("disabled" , true);
+      item.parentElement.parentElement.parentElement.classList.add('d-none');
+    });
+    clickItem.removeAttribute('disabled');
+    // 抓取課程
+    courseStr = clickItem.getAttribute('data-course');
+    courseTitle.textContent = `${courseStr}課程-基礎 `;
+    clickCard.classList.add('active');
+    clickCard.parentElement.classList.remove('d-none');
+
+  }else{
+    choose.classList.add('d-none');
+    allBtn.forEach((item)=>{
+      item.removeAttribute('disabled');
+      item.parentElement.parentElement.parentElement.classList.remove('d-none');
+    });
+    clickCard.classList.remove('active');
+  }
+};
+//reservationSwiper
+function reserveSwiperFn(){
+  let clickItem = this;
+  let activeItem = getActive(allItem);
+  let text = clickItem.dataset.text;
+  if(clickItem.classList.contains("active")){
+    clickItem.classList.remove('active');
+    choose.classList.add('disabled');
+  }
+  else{
+    if (typeof activeItem === "object") {
+      activeItem.classList.remove('active');
+    }
+    clickItem.classList.add('active');
+    choose.classList.remove('disabled');
+    courseTitle.textContent = `${courseStr}課程-${text} `;
+  }
+};
+function getActive(allItem){
+  let ary = Array.from(allItem).find((item) => {
+    return item.classList.contains('active');
+  });
+    return (ary === undefined) ? false : ary;
+};
 let reservationSwiper = new Swiper(".reservationSwiper", {
   slidesPerView: "auto",
   spaceBetween: 24,
@@ -86,14 +163,4 @@ let reservationSwiper = new Swiper(".reservationSwiper", {
     },
   },
 });
-
-
-// 瑜珈空間
-let envRecommendSwiper = new Swiper(".envRecommendSwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-});
-
-
-
 
