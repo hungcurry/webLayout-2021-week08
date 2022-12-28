943063d14f1d004bd8da9c51db5f4a01ab4e54b2
hungcurry <ooopp42@gmail.com>
2022-12-28 21:06:27 +0800

    [ edit ] 專案說明與js修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -47,7 +47,6 @@ let recommendSwiper = new Swiper(".recommendSwiper", {
   },
 });
 
-
 // 課程介紹
 let courseIntroduceSwiper = new Swiper(".courseIntroduceSwiper", {
   slidesPerView: "auto",
@@ -65,13 +64,11 @@ let courseIntroduceSwiper = new Swiper(".courseIntroduceSwiper", {
   },
 });
 
-
 // 瑜珈空間
 let envRecommendSwiper = new Swiper(".envRecommendSwiper", {
   slidesPerView: "auto",
   spaceBetween: 30,
 });
-
 
 // 立即預約
 const allBtn = document.querySelectorAll('.js-schemeBtn');
@@ -82,6 +79,7 @@ const courseTitle = document.querySelector('.js-courseTitle');
 const choose = document.querySelector('.js-choose');
 let courseStr = "";
 
+// 避免其他頁出錯誤
 if(allBtn && allItem && choose){
   choose.classList.add('d-none');
   allBtn.forEach((item)=>{
@@ -93,15 +91,17 @@ if(allBtn && allItem && choose){
 }
 // collapse
 function reserveCollapseFn(){
-  let clickItem = this ;
-  let clickCard = clickItem.parentElement.parentElement;
+  let clickItem = this ; // button
+  let clickCard = clickItem.parentElement.parentElement; // card
   let isExpanded = clickItem.getAttribute('aria-expanded');
 
   if(isExpanded === 'true'){ 
     choose.classList.remove('d-none');
+    // LI 全部消失
     allBtn.forEach((item)=>{
+      let allLI = item.parentElement.parentElement.parentElement;
       item.setAttribute("disabled" , true);
-      item.parentElement.parentElement.parentElement.classList.add('d-none');
+      allLI.classList.add('d-none');
     });
     clickItem.removeAttribute('disabled');
     // 抓取課程
@@ -111,13 +111,12 @@ function reserveCollapseFn(){
     clickCard.parentElement.classList.remove('d-none');
     // 滾到選擇課程階級
     scrollToCollapseFn();
-
-
   }else{
     choose.classList.add('d-none');
     allBtn.forEach((item)=>{
+      let allLI = item.parentElement.parentElement.parentElement;
       item.removeAttribute('disabled');
-      item.parentElement.parentElement.parentElement.classList.remove('d-none');
+      allLI.classList.remove('d-none');
     });
     clickCard.classList.remove('active');
   }
@@ -127,11 +126,18 @@ function reserveSwiperFn(){
   let clickItem = this;
   let activeItem = getActive(allItem);
   let text = clickItem.dataset.text;
+
   if(clickItem.classList.contains("active")){
     clickItem.classList.remove('active');
     choose.classList.add('disabled');
   }
   else{
+    // 方法一: 全部移除
+    // allItem.forEach((item)=>{
+    //   item.classList.remove('active');
+    //   choose.classList.add('disabled');
+    // }); 
+    // 方法二: 有active單一移除
     if (typeof activeItem === "object") {
       activeItem.classList.remove('active');
     }
@@ -149,8 +155,9 @@ function getActive(allItem){
 function scrollToCollapseFn(){
   let top = swiperOuter.offsetTop;
   window.scrollTo({
-    top: top -74, 
-    behavior: 'smooth'});
+    top: top -80, 
+    behavior: 'smooth'
+  });
 };
 let reservationSwiper = new Swiper(".reservationSwiper", {
   slidesPerView: "auto",
@@ -165,7 +172,7 @@ let reservationSwiper = new Swiper(".reservationSwiper", {
         rows: 0,
       },
     },
-    768:{
+    1024:{
       spaceBetween: 20,
       grid: {
         rows: 0,
@@ -175,20 +182,20 @@ let reservationSwiper = new Swiper(".reservationSwiper", {
 });
 
 
-  // scroll
-  const backBtn = document.querySelector('.topBack');
-  backBtn.addEventListener('click',scrollToTopFn);
-  window.addEventListener('scroll', scrollFn);
-  function scrollFn(){
-    let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-    if(scrollTop >= 40){
-      backBtn.classList.add('show');
-    }else{
-      backBtn.classList.remove('show');
-    }
-  };
-  function scrollToTopFn(){
-    window.scrollTo({
-      top: 0, 
-      behavior: 'smooth'});
-  };
+// scroll
+const backBtn = document.querySelector('.topBack');
+backBtn.addEventListener('click',scrollToTopFn);
+window.addEventListener('scroll', scrollFn);
+function scrollFn(){
+  let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+  if(scrollTop >= 40){
+    backBtn.classList.add('show');
+  }else{
+    backBtn.classList.remove('show');
+  }
+};
+function scrollToTopFn(){
+  window.scrollTo({
+    top: 0, 
+    behavior: 'smooth'});
+};
